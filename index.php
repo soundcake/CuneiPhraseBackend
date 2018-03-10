45aef387fd18e02b415033d661651af2aaaf75ad
graymouser <graymouser@purplejelly.org>
2018-03-10 14:43:06 +0000

    db conn

--- index.php
+++ index.php
@@ -6,6 +6,41 @@
  * Time: 13:15
  */
 
+array(
+  'Paper supports page',
+  'Paper disagrees with page',
+  '',
+);
+
+/**
+ * @var $servername
+ * @var $username
+ * @var $password
+ * @var $dbname
+ */
+extract(parse_ini_file('config.ini'));
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM academic_paper";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
 ?>
 
 <h1>CuneiPhrase</h1>
