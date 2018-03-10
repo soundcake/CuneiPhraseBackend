2cc5462220ff850b9b29a5f0fdc355a35a86c3e7
soundcake <luke@soundcake.co.uk>
2018-03-10 19:31:02 +0000

    w.i.p.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $.get("https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=chicken%20open_access:y&format=json&resulttype=core", function (data) {
+    $.get("https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=chicken&format=json&resulttype=core", function (data) {
         $("#search_results").html(data);
 
     });
