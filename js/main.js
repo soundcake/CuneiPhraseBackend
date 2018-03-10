d0a8b7e850d700dfdf949f0519074efa80c9705b
soundcake <luke@soundcake.co.uk>
2018-03-10 20:38:33 +0000

    w.i.p.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
-    $.get("https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=chicken%20open_access:y&format=json&resulttype=core", function (data) {
-        //$("#search_results").html(data);
-        //alert('done');
-        var parsedData = parseJSON(data);
-        $("#search_results").html(parsedData);
+    $.getJSON("https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=chicken%20open_access:y&format=json&resulttype=core", function (data) {
+        var i = 0;
+        $("#search_results").append('<ul>');
+        $.each(data['resultList']['result'], function (key, val) {
+            if (i < 5) {
+                var url;
+                var doi = val['doi'];
+                var title = val['title'];
+                $.each(val['fullTextUrlList'], function (newKey, value) {
+                    url = value[0]['url'];
+                    //$("#search_results").append('<p>url: ' + value[0]['url'] + ' --- title: ' + title + ' --- doi: ' + doi + '</p>');
+                    $("#search_results").append('<li>' +
+                        '<a href="' + url + '" data-indext="' + doi + '" target="_blank">' + title + '</a>' +
+                        '</li>');
+                });
+                i++;
+            }
+        });
+        $("#search_results").append('</ul>');
+
     });
 });
