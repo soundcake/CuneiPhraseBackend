8197e8742d286d92e737cf65320db1acd91e8d76
soundcake <luke@soundcake.co.uk>
2018-03-11 04:15:38 +0000

    w.i.p.

--- popup.php
+++ popup.php
@@ -136,7 +136,6 @@ if ($conn->connect_error) {
 
     <script src="/js/jquery.js"></script>
     <script src="/js/main.js"></script>
-    <script src="/js/other.js"></script>
 
 </head>
 <body id="main_body" data-index="<?php echo $currentPageUrl; ?>">
